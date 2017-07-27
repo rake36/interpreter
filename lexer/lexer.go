c182d4d5c843a25bae52c1b7c5dff9a56d0a7732
Matt Rakestraw <rake36@gmail.com>
2017-07-26 20:23:13 -0400

    lexer for identifiers and integers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -12,14 +12,14 @@ type Lexer struct {
 	ch           byte
 }
 
-// New ...
+// New ... Build a new Lexer
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
-// readChar ...
+// readChar get a single character and advance position by 1
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -30,9 +30,45 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
+// We only care about integers for now
+//   floats, hex, are not supported...
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// readIdentifier ...
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// isLetter is this byte an ASCII character or underscore?
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 // NextToken ...
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -53,6 +89,20 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// Don't need to read another char, so short-circuit
+			return tok
+		}
+		if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			// Don't need to read another char, so short-circuit
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 
 	l.readChar()
--- token/token.go
+++ token/token.go
@@ -2,13 +2,16 @@
 
 package token
 
+// TokenType defines what kind of entity it is
 type TokenType string
 
+// Token defines a single entity in our language
 type Token struct {
 	Type    TokenType
 	Literal string
 }
 
+// Constants ...
 const (
 	ILLEGAL   = "ILLEGAL"
 	EOF       = "EOF"
@@ -25,3 +28,16 @@ const (
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent ...
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
